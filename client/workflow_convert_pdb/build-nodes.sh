391a97b030b7cd6715170c93e00e1fc171b23aab
Aaron Virshup <aaronvirshup@sfogzpfxz1.ads.autodesk.com>
2016-11-12 13:12:03 -0800

    Move docker build into subdirectory

--- client/workflow_convert_pdb/build-nodes.sh
+++ client/workflow_convert_pdb/build-nodes.sh
@@ -4,6 +4,8 @@
 #   2) automatically updates the node definitions in nodes/ based on the 
 #      definitions in the mdtscripts/mdscripts.py file
 
+basepath=`pwd`
+cd mdtscripts && docker build . -t mdtscripts
 
-docker build . -t mdtscripts
-python mdtscripts/mdtscripts.py --writenodes
+cd $basepath && python mdtscripts/mdtscripts.py --writenodes
+
