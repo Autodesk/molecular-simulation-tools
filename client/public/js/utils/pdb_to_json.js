ca932bef17ed4d0b65bc6baa88ab53bf3fa12815
Justin McCandless <justinjmccandless@gmail.com>
2016-11-17 16:13:21 -0500

    Partial line parsing for atoms with spec

--- client/public/js/utils/pdb_to_json.js
+++ client/public/js/utils/pdb_to_json.js
@@ -1,8 +1,21 @@
 const pdbToJson = {
   convert(pdb) {
+    return {};
   },
 
   parseLine(line) {
+    //const firstWordRX = /[A-Z0-9]* /;
+    const atomRX = /ATOM /;
+
+    if (atomRX.test(line)) {
+      return pdbToJson.parseAtom(line);
+    }
+
+    throw new Error(`Invalid line in PDB: ${line}`);
+  },
+
+  parseAtom(line) {
+    return {};
   },
 };
 
--- client/test/pdb_to_json_spec.js
+++ client/test/pdb_to_json_spec.js
@@ -1,8 +1,39 @@
-import {assert} from 'chai';
+import { expect } from 'chai';
 import pdbToJson from '../public/js/utils/pdb_to_json';
 
 describe('pdbToJson', () => {
   it('should be an object', () => {
-    assert.equal(typeof pdbToJson, 'object');
+    expect(typeof pdbToJson).to.equal('object');
+  });
+
+  describe('parseLine', () => {
+    let line;
+
+    describe('when given a valid atom line', () => {
+      beforeEach(() => {
+        line = 'ATOM      1  N   PRO A   1      -2.555   9.253  34.411  1.00 30.60           N ';
+      });
+
+      it('returns an atom object', () => {
+        const parsedLine = pdbToJson.parseLine(line);
+
+        expect(typeof parsedLine).to.equal('object');
+      });
+    });
+
+    describe('when given an invalid line', () => {
+      beforeEach(() => {
+        line = 'IM A PDB LINE OR SOMETHING';
+      });
+
+      it('throws an error', () => {
+        const parsedLine = pdbToJson.parseLine(line);
+
+        console.log('do it');
+        expect(() => {
+          pdbToJson.parseLine(line);
+        }).to.throw(Error);
+      });
+    });
   });
 });
