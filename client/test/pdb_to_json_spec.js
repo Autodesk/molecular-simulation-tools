d5dd5296f4aff8a752ed3a9fd41085e849ae585e
Justin McCandless <justinjmccandless@gmail.com>
2016-11-23 12:21:46 -0800

    Rearrange pdb spec to use spy

--- client/test/pdb_to_json_spec.js
+++ client/test/pdb_to_json_spec.js
@@ -1,12 +1,39 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
 import pdbToJson from '../public/js/utils/pdb_to_json';
 
 describe('pdbToJson', () => {
-  it('should be an object', () => {
-    expect(typeof pdbToJson).to.equal('object');
+  describe('parseLine', () => {
+    let line;
+
+    describe('when given a valid atom line', () => {
+      beforeEach(() => {
+        line = 'ATOM      1  N   PRO A   1      -2.555   9.253  34.411  1.00 30.60           N ';
+      });
+
+      it('calls parseAtom', () => {
+        const parseAtomSpy = sinon.spy(pdbToJson, 'parseAtom');
+
+        pdbToJson.parseLine(line);
+
+        expect(pdbToJson.parseAtom.called).to.equal(true);
+
+        parseAtomSpy.restore();
+      });
+    });
+
+    describe('when given an invalid line', () => {
+      beforeEach(() => {
+        line = 'IM A PDB LINE OR SOMETHING';
+      });
+
+      it('throws an error', () => {
+        expect(pdbToJson.parseLine.bind(null, line)).to.throw(Error);
+      });
+    });
   });
 
-  describe('parseLine', () => {
+  describe('parseAtom', () => {
     let line;
 
     describe('when given a valid atom line', () => {
@@ -25,16 +52,6 @@ describe('pdbToJson', () => {
         expect(parsedLine.positions[1]).to.equal(9.253);
         expect(parsedLine.positions[2]).to.equal(34.411);
         expect(parsedLine.residueIndex).to.equal(1);
-      });
-    });
-
-    describe('when given an invalid line', () => {
-      beforeEach(() => {
-        line = 'IM A PDB LINE OR SOMETHING';
-      });
-
-      it('throws an error', () => {
-        expect(pdbToJson.parseLine.bind(null, line)).to.throw(Error);
       });
     });
   });
