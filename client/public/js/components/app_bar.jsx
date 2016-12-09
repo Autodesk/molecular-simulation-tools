eb0ad484144c224f7405f998de957a1945fadf5d
Justin McCandless <justinjmccandless@gmail.com>
2016-12-08 16:05:32 -0800

    Share popover menu

--- client/public/js/components/app_bar.jsx
+++ client/public/js/components/app_bar.jsx
@@ -1,22 +1,64 @@
 import React from 'react';
-import Menu from 'material-ui/Menu';
+import Popover from 'material-ui/Popover';
+import MenuItem from 'material-ui/MenuItem';
 import logoImage from '../../img/logo.png';
 
 require('../../css/app_bar.scss');
 
-function AppBar(props) {
-  return (
-    <div className="app-bar">
-      <img src={logoImage} alt="logo" className="logo" />
-      <div className="title">
-        <h2>{props.title}</h2>
+class AppBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onClickShare = this.onClickShare.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+
+    this.state = {
+      shareMenuOpen: false,
+    };
+  }
+
+  onClickShare() {
+    this.setState({
+      shareMenuOpen: true,
+    });
+  }
+
+  handleRequestClose() {
+    this.setState({
+      shareMenuOpen: false,
+    });
+  }
+
+  render() {
+    return (
+      <div className="app-bar">
+        <img src={logoImage} alt="logo" className="logo" />
+        <div className="title">
+          <h2>{this.props.title}</h2>
+        </div>
+        <button>
+          Browse all tools
+        </button>
+        <Popover
+          open={this.state.shareMenuOpen}
+          anchorEl={this.shareButton}
+          onRequestClose={this.handleRequestClose}
+        >
+          <MenuItem>Email</MenuItem>
+          <MenuItem>Twitter</MenuItem>
+          <MenuItem>Facebook</MenuItem>
+        </Popover>
+        <button
+          ref={(c) => { this.shareButton = c; }}
+          onClick={this.onClickShare}
+        >
+          Share
+        </button>
+        <button>Join</button>
+        <button>Help</button>
       </div>
-      <button>Browse all tools</button>
-      <button>Share</button>
-      <button>Join</button>
-      <button>Help</button>
-    </div>
-  );
+    );
+  }
 }
 
 AppBar.propTypes = {
