c8af8c1c4169f4d8fdb1df4b9108142b69bfec7f
Justin McCandless <justinjmccandless@gmail.com>
2016-12-15 14:07:39 -0800

    Server improvements from express generator, and get workflow endpoint with fake data

--- server/server.js
+++ server/server.js
@@ -1,29 +1,47 @@
-
-/********************************
- * Get external variables and parameters
- ********************************/
-var PORT = process.env["PORT"] && process.env["PORT"] != "" ? parseInt(process.env["PORT"]) : 4000;
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const appConstants = require('./constants/app_constants');
+const workflowRouter = require('./workflow');
 
 /********************************
  * Create the server
  ********************************/
-var express = require('express');
 var app = express();
+
+/*******************************
+ * Express boilerplate middlewares
+ ********************************/
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+/* Serve client files */
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 /********************************
  * Add server routes
  ********************************/
+app.use(`${appConstants.VERSION_PREFIX}/workflow`, workflowRouter);
 
-var workflowRouter = require('./workflow');
-app.use(workflowRouter);
-
-/* Serve client files */
-app.use(express.static('client/dist'));
-
-/********************************
- * Start the server
- ********************************/
-
-app.listen(PORT, function () {
-  console.log('Server listening on 0.0.0.0:' + PORT);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // return error json, only providing error in development
+  res.status(err.status || 500);
+  return res.send({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {},
+  });
+});
+
+module.exports = app;
