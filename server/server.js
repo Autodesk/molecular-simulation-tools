37c5e99468718d6810e6b27b4c332809d5512703
Justin McCandless <justinjmccandless@gmail.com>
2016-12-16 10:35:36 -0800

    Add airbnb eslint and fix errors in server.js

--- server/server.js
+++ server/server.js
@@ -6,36 +6,30 @@ const path = require('path');
 const appConstants = require('./constants/app_constants');
 const workflowRouter = require('./workflow');
 
-/********************************
- * Create the server
- ********************************/
-var app = express();
+// Create the server
+const app = express();
 
-/*******************************
- * Express boilerplate middlewares
- ********************************/
+// Express boilerplate middlewares
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-/* Serve client files */
+// Serve client files
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
-/********************************
- * Add server routes
- ********************************/
+// Add server routes
 app.use(`${appConstants.VERSION_PREFIX}/workflow`, workflowRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // return error json, only providing error in development
   res.status(err.status || 500);
   return res.send({
