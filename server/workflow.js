1a8ebb1caffe01767642cb5add228d861af7ea31
Justin McCandless <justinjmccandless@gmail.com>
2016-12-16 10:42:34 -0800

    workflow.js passes eslint

--- server/workflow.js
+++ server/workflow.js
@@ -1,10 +1,11 @@
-/********************************
- * /workflow routes
- ********************************/
-var express = require('express');
-var router = express.Router();
+/**
+ * workflow routes
+ */
+const express = require('express');
 
-var WORKFLOW_SERVER_ADDRESS = "localhost:9000";
+// const WORKFLOW_SERVER_ADDRESS = 'localhost:9000';
+
+const router = new express.Router();
 
 router.get('/:workflowId', (req, res, next) => {
   if (!req.params.workflowId) {
@@ -21,36 +22,38 @@ router.get('/:workflowId', (req, res, next) => {
     ],
   };
 
-  res.send(fakeWorkflowData);
+  return res.send(fakeWorkflowData);
 });
 
 /**
  * This route can be polled
  */
-router.get('/status/:workflowId', function (req, res) {
-	// console.log("originalUrl=" + req.originalUrl); // '/admin/new'
-	// console.log("baseUrl=" + req.baseUrl); // '/admin'
-	// console.log("path=" + req.path); // '/new'
-	// console.log("params=" + JSON.stringify(req.params)); // '/new'
-	// res.send('Workflow ' + JSON.stringify({
-	// 	originalUrl: req.originalUrl,
-	// 	baseUrl: req.baseUrl,
-	// 	path: req.path,
-	// 	params: req.params,
-	// 	originalUrl: req.originalUrl,
-	// 	route: req.route,
-	// 	url: req.url,
-	// }, null, "  "));
-	res.send({workflowId:req.params.workflowId, status:"running(this is a test)"});
+router.get('/status/:workflowId', (req, res) => {
+  // console.log("originalUrl=" + req.originalUrl); // '/admin/new'
+  // console.log("baseUrl=" + req.baseUrl); // '/admin'
+  // console.log("path=" + req.path); // '/new'
+  // console.log("params=" + JSON.stringify(req.params)); // '/new'
+  // res.send('Workflow ' + JSON.stringify({
+  //   originalUrl: req.originalUrl,
+  //   baseUrl: req.baseUrl,
+  //   path: req.path,
+  //   params: req.params,
+  //   originalUrl: req.originalUrl,
+  //   route: req.route,
+  //   url: req.url,
+  // }, null, "  "));
+  res.send({
+    workflowId: req.params.workflowId,
+    status: 'running(this is a test)',
+  });
 });
 
-router.post('/run', function (req, res) {
-	res.send('MOCKED API: Attempting to run. This will be filled in later by Dion');
+router.post('/run', (req, res) => {
+  res.send('MOCKED API: Attempting to run. This will be filled in later by Dion');
 });
 
-router.get('/run', function (req, res) {
-	res.send('MOCKED API: run some workflow. Actually, you will use the POST method');
+router.get('/run', (req, res) => {
+  res.send('MOCKED API: run some workflow. Actually, you will use the POST method');
 });
 
-
-module.exports = router
+module.exports = router;
