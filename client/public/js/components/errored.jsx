15f7014f0d346cfad13e9f9005903da3496fc650
Justin McCandless <justinjmccandless@gmail.com>
2017-01-11 16:26:16 -0800

    Malte's new error page UI

--- client/public/js/components/errored.jsx
+++ client/public/js/components/errored.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import IncompleteFooter from './incomplete_footer';
 
 import '../../css/incomplete.scss';
@@ -6,20 +7,18 @@ import '../../css/incomplete.scss';
 function Errored() {
   return (
     <div className="errored">
+      <div className="image1" />
+      <div className="image2" />
+      <div className="image3" />
       <div className="container">
-        <div>
-          <h1>An error occured</h1>
-          <div className="line" />
-        </div>
+        <h1>An error occurred</h1>
         <p>
           We are sorry, but we are unable to process your simulation.
         </p>
         <p>
           Our team has been notified and will try to solve the problem.
         </p>
-        <button type="button" className="largeButton">
-          Try different tool
-        </button>
+        <Link to="/" className="largeButton">Try different tool</Link>
       </div>
       <IncompleteFooter />
     </div>
