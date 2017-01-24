ab661b8478fcf1c8567700e26f52ede614f1d5dc
Justin McCandless <justinjmccandless@gmail.com>
2017-01-24 14:15:58 -0800

    Fix workflow loading card

--- client/public/js/components/workflow_card_loading.jsx
+++ client/public/js/components/workflow_card_loading.jsx
@@ -4,13 +4,11 @@ import '../../css/workflow_card.scss';
 function WorkflowCardLoading() {
   return (
     <div className="workflow-card workflow-card-loading col-md-4 col-sm-6 col-xs-12">
-      <div className="card">
-        <div
-          className="cardBack"
-        />
-        <div className="cardOverlay" />
-        <div className="cardLogo" />
-      </div>
+      <div
+        className="bg-image"
+      />
+      <div className="cardLogo" />
+      <div className="cardOverlay" />
     </div>
   );
 }
