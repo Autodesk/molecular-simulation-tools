7b0365dbc78fe487830e796222bb4f6332830ab5
Justin McCandless <justinjmccandless@gmail.com>
2017-02-03 15:38:28 -0800

    Handle molviewer timing using events

--- client/public/js/components/view.jsx
+++ client/public/js/components/view.jsx
@@ -5,6 +5,9 @@ import loadImg from '../../img/loadAnim.gif';
 
 require('../../css/view.scss');
 
+const MOL_VIEW_INITIALIZED = 'viewerInitialized';
+const MOL_VIEW_MODEL_LOADED = 'Nano.ModelEndLoaded';
+
 class View extends React.Component {
   componentDidMount() {
     this.renderMolecueViewer(this.props.modelData, this.props.selectionStrings);
@@ -15,49 +18,27 @@ class View extends React.Component {
   }
 
   renderMolecueViewer(modelData, selectionStrings) {
-    let molViewerPromise;
-
     if (modelData && !this.moleculeViewer) {
-      // TODO async promise hack, molviewer should be updated to give a better
-      // way to let me know it can be used
-      molViewerPromise = new Promise((resolve, reject) => {
-        this.moleculeViewer = new $ADSKMOLVIEW(this.moleculeViewerContainer, {
-          headless: true,
-        });
-        if (this.moleculeViewer) {
-          resolve(this.moleculeViewer);
-        } else {
-          reject(new Error('Failed creating app'));
-        }
-      }).then(() => {
-        window.moleculeViewer = this.moleculeViewer;
-        this.moleculeViewer.createMoleculeFromFile(modelData, 'pdb');
-      });
-
-      /*
       this.moleculeViewer = new $ADSKMOLVIEW(this.moleculeViewerContainer, {
         headless: true,
       });
-      try {
+
+      this.moleculeViewer.mv.addEventListener(MOL_VIEW_INITIALIZED, () => {
         this.moleculeViewer.createMoleculeFromFile(modelData, 'pdb');
-      } catch (err) {
-        console.error(err);
-      }
-      */
+      });
     }
 
     if (modelData && this.moleculeViewer) {
       if (selectionStrings) {
-        // TODO Fix bug in molviewer that requires this promise AND timeout
-        molViewerPromise.then(() => {
-          setTimeout(() => {
+        this.moleculeViewer.mv.addEventListener(
+          MOL_VIEW_MODEL_LOADED, () => {
             this.moleculeViewer.clearSelection();
             selectionStrings.forEach(selectionString =>
               this.moleculeViewer.select(selectionString)
             );
             this.moleculeViewer.focusOnSelection();
-          }, 500);
-        });
+          }
+        );
       }
     }
 
