bd152b9507489448d419471837661471f1aab8d6
Justin McCandless <justinjmccandless@gmail.com>
2017-02-09 17:26:22 -0800

    Ligand selection in molviewer and status pane

--- client/public/js/utils/io_utils.js
+++ client/public/js/utils/io_utils.js
@@ -1,3 +1,5 @@
+import { List as IList } from 'immutable';
+
 const ioUtils = {
   /**
    * Given a list of inputs, find the pdb io and get the pdb data.
@@ -24,6 +26,33 @@ const ioUtils = {
    */
   getIndexByExtension(ios, extension) {
     return ios.findIndex(io => io.value.endsWith(extension));
+  },
+
+  /**
+   * Given a list of ios, find and return a list of all ligands in results data
+   * @param ios {IList}
+   * @returns {IList}
+   */
+  getLigandNames(ios) {
+    return ios.reduce((reduction, io) => {
+      if (!io.value.endsWith('.json')) {
+        return reduction;
+      }
+      const ligandNames = new IList(Object.keys(io.fetchedValue.ligands));
+      return reduction.concat(ligandNames);
+    }, new IList());
+  },
+
+  getLigandSelectionStrings(ios, ligandName) {
+    const ioWithLigand = ios.find((io) => {
+      if (!io.value.endsWith('.json')) {
+        return false;
+      }
+
+      return io.fetchedValue.mv_ligand_strings[ligandName];
+    });
+
+    return ioWithLigand.fetchedValue.mv_ligand_strings[ligandName];
   },
 
   /**
