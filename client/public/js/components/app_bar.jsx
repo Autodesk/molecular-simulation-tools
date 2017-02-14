2ddc609fa310cec0f3912443920510f4f10357cf
Justin McCandless <justinjmccandless@gmail.com>
2017-02-14 11:07:39 -0800

    No more share in app bar for now

--- client/public/js/components/app_bar.jsx
+++ client/public/js/components/app_bar.jsx
@@ -60,6 +60,7 @@ class AppBar extends React.Component {
               <img className="shareButton" src={shareFacebookImage} alt="facebook" />
             </div>
           </Popover>
+          {/*
           <button
             className="appLink"
             ref={(c) => { this.shareButton = c; }}
@@ -67,6 +68,7 @@ class AppBar extends React.Component {
           >
             Share
           </button>
+          */}
           <Link className="appLink" to="/#join">Join</Link>
           <button className="appLink" >Help</button>
         </div>
