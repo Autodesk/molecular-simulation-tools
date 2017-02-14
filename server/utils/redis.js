5f7af7c1058af809900e5cf54cc13f02d9a2ab69
Dion Amago <dion@transition9.com>
2017-02-14 10:32:35 -0800

    No local redis cache for prod machines

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -3,6 +3,9 @@ const promiseRedis = require('promise-redis');
 const Redis = promiseRedis(resolver =>
   new Promise(resolver)
 );
-const redis = Redis.createClient({ host: 'redis', port: 6379 });
+
+const REDIS_HOST = process.env.REDIS_HOST;
+
+const redis = Redis.createClient({ host: REDIS_HOST, port: 6379 });
 
 module.exports = redis;
