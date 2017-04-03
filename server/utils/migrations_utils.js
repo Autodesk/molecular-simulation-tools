984826bef6b50fe31803cbf8f75ff6feff511b7e
Justin McCandless <justinjmccandless@gmail.com>
2017-04-03 11:56:38 -0700

    Fix first migration error due to undefined global log

--- server/utils/migrations_utils.js
+++ server/utils/migrations_utils.js
@@ -1,4 +1,5 @@
 const dbConstants = require('../constants/db_constants');
+const log = require('./log');
 
 const OLD_REDIS_APPS = 'workflows';
 
@@ -7,7 +8,7 @@ const migrationsUtils = {
    * Rename an old field in each run to the new name
    */
   migrateRunsWorkflowField(redis) {
-    global.log.debug('Migrating runs workflow fields...');
+    log.debug('Migrating runs workflow fields...');
 
     return redis.hgetall(dbConstants.REDIS_RUNS).then((runsHash) => {
       if (!runsHash) {
@@ -18,24 +19,24 @@ const migrationsUtils = {
         const run = JSON.parse(runString);
 
         if (run.appId) {
-          global.log.debug(`Not migrating run ${run.id} because already has appId`);
+          log.debug(`Not migrating run ${run.id} because already has appId`);
           return Promise.resolve();
         }
 
         return redis.hset(dbConstants.REDIS_RUNS, run.id, JSON.stringify(Object.assign({}, run, {
           appId: run.workflowId,
         }))).then(() => {
-          global.log.debug(`Finished migrating run ${run.id} workflow field.`);
-        }).catch(global.log.error);
+          log.debug(`Finished migrating run ${run.id} workflow field.`);
+        }).catch(log.error);
       }));
-    }).catch(global.log.error);
+    }).catch(log.error);
   },
 
   /*
    * Move workflows from the old workflow table into the app table
    */
   migrateWorkflowsToApps(redis) {
-    global.log.debug('Migrating workflows to apps...');
+    log.debug('Migrating workflows to apps...');
 
     return redis.hgetall(OLD_REDIS_APPS).then((oldAppsHash) => {
       Object.values(oldAppsHash).forEach((oldAppString) => {
@@ -43,16 +44,16 @@ const migrationsUtils = {
 
         return redis.hget(dbConstants.REDIS_APPS, oldApp.id).then((appString) => {
           if (appString) {
-            global.log.debug(`Did not migrate ${oldApp.id} because app exists.`);
+            log.debug(`Did not migrate ${oldApp.id} because app exists.`);
             return Promise.resolve();
           }
 
           return redis.hset(dbConstants.REDIS_APPS, oldApp.id, JSON.stringify(oldApp)).then(() => {
-            global.log.debug(`Finished migrating workflow ${oldApp.id} to app.`);
-          }).catch(global.log.error);
-        }).catch(global.log.error);
+            log.debug(`Finished migrating workflow ${oldApp.id} to app.`);
+          }).catch(log.error);
+        }).catch(log.error);
       });
-    }).catch(global.log.error);
+    }).catch(log.error);
   },
 };
 
