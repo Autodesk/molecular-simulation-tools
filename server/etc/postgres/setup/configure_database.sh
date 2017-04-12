aafb5753893b3886b690afceb3acda20b8136291
Peter Jones <peter.jones@autodesk.com>
2017-04-12 15:58:56 -0700

    changed database create flow and fixed parameter to GRANT

--- server/etc/postgres/setup/configure_database.sh
+++ server/etc/postgres/setup/configure_database.sh
@@ -2,7 +2,27 @@
 
 # Create user
 dropuser -U postgres -w --if-exists mstdbuser
-createuser -U postgres -w --no-password -E -i -l -r mstdbuser
+createuser -U postgres -w --no-password -d -E -i -l -r -s mstdbuser
+
+# see if db exists
+set +e
+# Good way to determine if db exists : http://stackoverflow.com/questions/14549270/check-if-database-exists-in-postgresql-using-shell
+echo "databases found:"
+psql -lqt | cut -d \| -f 1 
+echo "psql -lqt | cut -d \| -f 1 | grep -qw mstdbv1"
+psql -lqt | cut -d \| -f 1 | grep -qw mstdbv1
+dbExists=$?
+# for some reason this command below did not work like above
+# dbExists=$(psql -lqt | cut -d \| -f 1 | grep -qw mstdbv1)
+echo "dbExists=$dbExists (0=exists, 1=not exist)"
+# returns 0 if db  exists, and 1 if it DOES NOT exists
+
+set -e
+if [ "$dbExists" = "1" ]
+then
+  psql -a -v ON_ERROR_STOP=1 -d postgres -f /create_database.sql
+fi
+
 
 # Create database
 # this happens automatically when the .sql file is copied into /docker-entrypoint-initdb.d/
