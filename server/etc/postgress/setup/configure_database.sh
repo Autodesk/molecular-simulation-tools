babda3d18e2f05b47db84f94ba07e0982c9154c0
Peter Jones <peter.jones@autodesk.com>
2017-04-12 10:39:48 -0700

    make role and database names lower case

--- server/etc/postgress/setup/configure_database.sh
+++ server/etc/postgress/setup/configure_database.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 # Create user
-dropuser -U postgres -w --if-exists mstDBUser
-createuser -U postgres -w --no-password -d -E -i -l -r -s mstDBUser
+dropuser -U postgres -w --if-exists mstdbuser
+createuser -U postgres -w --no-password -E -i -l -r mstdbuser
 
 # Create database
 # this happens automatically when the .sql file is copied into /docker-entrypoint-initdb.d/
