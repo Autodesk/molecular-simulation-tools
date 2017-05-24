daeec0ca00f2ef243677936d145a8034fa927165
Dion Amago <dion@transition9.com>
2017-05-24 14:07:41 -0700

    Widget should start in IDLE status

--- client/public/js/records/widget_record.js
+++ client/public/js/records/widget_record.js
@@ -7,8 +7,7 @@ const WidgetRecord = new Record({
   config: new IMap(),
   inputPipes: new IList(),
   outputPipes: new IList(),
-  // TODO this status should be statusConstants.IDLE
-  status: statusConstants[Object.keys(statusConstants)[Math.round(Math.random() * 3)]],
+  status: statusConstants.IDLE,
   error: '',
 });
 
